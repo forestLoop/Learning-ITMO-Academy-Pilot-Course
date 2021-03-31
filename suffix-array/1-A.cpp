490ef9e6a2b7eac54104b8317cd791237617d1aa
Keith Null <keith1126@126.com>
2021-03-31 23:26:20 +0800

    Update format settings

--- suffix-array/1-A.cpp
+++ suffix-array/1-A.cpp
@@ -3,52 +3,52 @@
 using namespace std;
 
 vector<int> computeSuffixArray(string s) {
-  s += "$";
-  int n = s.size();
-  vector<int> p(n), c(n);
-  {
-    // k = 0
-    vector<pair<char, int>> a(n);
-    for (int i = 0; i < n; i++) {
-      a[i] = {s[i], i};
+    s += "$";
+    int n = s.size();
+    vector<int> p(n), c(n);
+    {
+        // k = 0
+        vector<pair<char, int>> a(n);
+        for (int i = 0; i < n; i++) {
+            a[i] = {s[i], i};
+        }
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++) {
+            p[i] = a[i].second;
+        }
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            c[p[i]] = c[p[i - 1]] + int(a[i - 1].first != a[i].first);
+        }
     }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n; i++) {
-      p[i] = a[i].second;
+    int k = 0;
+    while ((1 << k) < n && c[p[n - 1]] < n - 1) {  // for early termination
+        // k -> k + 1
+        vector<pair<pair<int, int>, int>> a(n);
+        for (int i = 0; i < n; i++) {
+            a[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
+        }
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++) {
+            p[i] = a[i].second;
+        }
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            c[p[i]] = c[p[i - 1]] + int(a[i - 1].first != a[i].first);
+        }
+        k++;
     }
-    c[p[0]] = 0;
-    for (int i = 1; i < n; i++) {
-      c[p[i]] = c[p[i - 1]] + int(a[i - 1].first != a[i].first);
-    }
-  }
-  int k = 0;
-  while ((1 << k) < n && c[p[n - 1]] < n - 1) {  // for early termination
-    // k -> k + 1
-    vector<pair<pair<int, int>, int>> a(n);
-    for (int i = 0; i < n; i++) {
-      a[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
-    }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n; i++) {
-      p[i] = a[i].second;
-    }
-    c[p[0]] = 0;
-    for (int i = 1; i < n; i++) {
-      c[p[i]] = c[p[i - 1]] + int(a[i - 1].first != a[i].first);
-    }
-    k++;
-  }
-  return p;
+    return p;
 }
 
 int main() {
-  string s;
-  while (cin >> s) {
-    vector<int> result = computeSuffixArray(s);
-    for (auto it = result.begin(); it != result.end(); it++) {
-      cout << *it << " ";
+    string s;
+    while (cin >> s) {
+        vector<int> result = computeSuffixArray(s);
+        for (auto it = result.begin(); it != result.end(); it++) {
+            cout << *it << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
+    return 0;
 }
--- suffix-array/3-A.cpp
+++ suffix-array/3-A.cpp
@@ -3,103 +3,103 @@
 using namespace std;
 
 void countSort(vector<int> &p, vector<int> &c) {
-  int n = p.size();
-  vector<int> cnt(n);
-  for (auto x : c) {
-    cnt[x]++;
-  }
-  vector<int> pos(n);
-  pos[0] = 0;
-  for (int x = 1; x < n; x++) {
-    pos[x] = pos[x - 1] + cnt[x - 1];
-  }
-  vector<int> p_new(n);
-  for (auto x : p) {
-    p_new[pos[c[x]]] = x;
-    pos[c[x]]++;
-  }
-  p = p_new;
+    int n = p.size();
+    vector<int> cnt(n);
+    for (auto x : c) {
+        cnt[x]++;
+    }
+    vector<int> pos(n);
+    pos[0] = 0;
+    for (int x = 1; x < n; x++) {
+        pos[x] = pos[x - 1] + cnt[x - 1];
+    }
+    vector<int> p_new(n);
+    for (auto x : p) {
+        p_new[pos[c[x]]] = x;
+        pos[c[x]]++;
+    }
+    p = p_new;
 }
 
 vector<int> computeSuffixArray(string s) {
-  s += "$";
-  int n = s.size();
-  vector<int> pos(n), class_(n);
-  // pos[i]: the i-th item's index, i.e., s[pos[i]:] is the i-th item in suffix
-  // array
-  // class_[i]: the equivalance class of s[i]
-  {
-    // k = 0
-    vector<pair<char, int>> a(n);
-    for (int i = 0; i < n; i++) {
-      a[i] = {s[i], i};
+    s += "$";
+    int n = s.size();
+    vector<int> pos(n), class_(n);
+    // pos[i]: the i-th item's index, i.e., s[pos[i]:] is the i-th item in suffix
+    // array
+    // class_[i]: the equivalance class of s[i]
+    {
+        // k = 0
+        vector<pair<char, int>> a(n);
+        for (int i = 0; i < n; i++) {
+            a[i] = {s[i], i};
+        }
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++) {
+            pos[i] = a[i].second;
+        }
+        class_[pos[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            class_[pos[i]] = class_[pos[i - 1]] + int(a[i - 1].first != a[i].first);
+        }
     }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n; i++) {
-      pos[i] = a[i].second;
+    int k = 0;
+    while ((1 << k) < n && class_[pos[n - 1]] < n - 1) {  // for early termination
+        // k -> k + 1
+        for (int i = 0; i < n; i++) {
+            // actually, this is the first counting sort of a radix sort
+            //   class_[i] = class_[(pos[i] - (1 << k) + n) % n]
+            pos[i] = (pos[i] - (1 << k) + n) % n;
+        }
+        countSort(pos, class_);
+        vector<int> class_new(n);
+        class_new[pos[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> prev = {class_[pos[i - 1]],
+                                   class_[(pos[i - 1] + (1 << k)) % n]};
+            pair<int, int> curr = {class_[pos[i]], class_[(pos[i] + (1 << k)) % n]};
+            class_new[pos[i]] = class_new[pos[i - 1]] + int(prev != curr);
+        }
+        class_ = class_new;
+        k++;
     }
-    class_[pos[0]] = 0;
-    for (int i = 1; i < n; i++) {
-      class_[pos[i]] = class_[pos[i - 1]] + int(a[i - 1].first != a[i].first);
-    }
-  }
-  int k = 0;
-  while ((1 << k) < n && class_[pos[n - 1]] < n - 1) {  // for early termination
-    // k -> k + 1
-    for (int i = 0; i < n; i++) {
-      // actually, this is the first counting sort of a radix sort
-      //   class_[i] = class_[(pos[i] - (1 << k) + n) % n]
-      pos[i] = (pos[i] - (1 << k) + n) % n;
-    }
-    countSort(pos, class_);
-    vector<int> class_new(n);
-    class_new[pos[0]] = 0;
-    for (int i = 1; i < n; i++) {
-      pair<int, int> prev = {class_[pos[i - 1]],
-                             class_[(pos[i - 1] + (1 << k)) % n]};
-      pair<int, int> curr = {class_[pos[i]], class_[(pos[i] + (1 << k)) % n]};
-      class_new[pos[i]] = class_new[pos[i - 1]] + int(prev != curr);
-    }
-    class_ = class_new;
-    k++;
-  }
-  return pos;
+    return pos;
 }
 
 bool countSubstr(const string &s, const vector<int> &suffix, const string &t) {
-  int lo = 0, hi = suffix.size();
-  while (lo < hi) {
-    auto mid = lo + (hi - lo) / 2;
-    // substr will take care of edge cases when the required sub string extends
-    // past the end
-    string sub = s.substr(suffix[mid], t.size());
-    if (sub == t) {
-      return true;
+    int lo = 0, hi = suffix.size();
+    while (lo < hi) {
+        auto mid = lo + (hi - lo) / 2;
+        // substr will take care of edge cases when the required sub string extends
+        // past the end
+        string sub = s.substr(suffix[mid], t.size());
+        if (sub == t) {
+            return true;
+        }
+        if (sub < t) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
     }
-    if (sub < t) {
-      lo = mid + 1;
-    } else {
-      hi = mid;
-    }
-  }
-  return false;
+    return false;
 }
 
 int main() {
-  string s;
-  while (cin >> s) {
-    vector<int> result = computeSuffixArray(s);
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-      string t;
-      cin >> t;
-      if (countSubstr(s, result, t)) {
-        cout << "Yes" << endl;
-      } else {
-        cout << "No" << endl;
-      }
+    string s;
+    while (cin >> s) {
+        vector<int> result = computeSuffixArray(s);
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            string t;
+            cin >> t;
+            if (countSubstr(s, result, t)) {
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }
+        }
     }
-  }
-  return 0;
+    return 0;
 }
